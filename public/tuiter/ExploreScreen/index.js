b46f4922709f8f82dc9766b2a2053f396864c5e5
gaotianh <gao.tianh99@gmail.com>
2022-10-16 02:43:20 -0400

    further missing js fixed

--- public/tuiter/ExploreScreen/index.js
+++ public/tuiter/ExploreScreen/index.js
@@ -1,6 +1,5 @@
 import NavigationSidebar from "../NavigationSidebar/index.js";
-import WhoToFollowList from "../WhoToFollowList.js";
-import PostSummaryList from "../PostSummaryList";
+import WhoToFollowList from "../WhoToFollowList/index.js";
 import ExploreComponent from "./ExploreComponent.js";
 function exploreComponent() {
     $('#wd-explore').append(`
